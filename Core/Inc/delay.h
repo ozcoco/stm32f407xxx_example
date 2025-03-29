929a985654607d2d088c1190d9e175a7ce3414a5
ozcomcn <857527916@qq.com>
2025-03-29 16:28:54 +0800

    基本程序框架搭建完成

---
+++ Core/Inc/delay.h
@@ -0,0 +1,14 @@
+//
+// Created by ozcom on 2024/1/8.
+//
+
+#ifndef FMC_TFT_LCD_DELAY_H
+#define FMC_TFT_LCD_DELAY_H
+
+#include "main.h"
+
+void delay_us(uint32_t us);
+
+void delay_ms(uint16_t ms);
+
+#endif //FMC_TFT_LCD_DELAY_H
---
+++ Core/Src/delay.c
@@ -0,0 +1,32 @@
+//
+// Created by ozcom on 2024/1/8.
+//
+#include "delay.h"
+
+void delay_us(uint32_t us)
+{
+    uint32_t temp;
+    SysTick->LOAD = HAL_RCC_GetHCLKFreq()/1000000/8*us;
+    SysTick->VAL = 0X00;
+    SysTick->CTRL = 0X01;
+    do {
+        temp = SysTick->CTRL;
+    }
+    while ((temp & 0x01) && (!(temp & (1 << 16))));
+    SysTick->CTRL = 0x00;
+    SysTick->VAL = 0X00;
+}
+
+void delay_ms(uint16_t ms)
+{
+    uint32_t temp;
+    SysTick->LOAD = HAL_RCC_GetHCLKFreq()/1000/8*ms;
+    SysTick->VAL = 0X00;
+    SysTick->CTRL = 0X01;
+    do {
+        temp = SysTick->CTRL;
+    }
+    while ((temp & 0x01) && (!(temp & (1 << 16))));
+    SysTick->CTRL = 0x00;
+    SysTick->VAL = 0X00;
+}
